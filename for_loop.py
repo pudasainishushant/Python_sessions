35bfbc7526e95091f0a380c10f626917950109b3
Shushant Pudasaini <shusrulz@gmail.com>
2020-09-22 19:49:51 +0545

    list_tupel_dict looping

---
+++ for_loop.py
@@ -0,0 +1,9 @@
+# This is a comment
+"""
+This is first line of comment
+This is second line of comment
+"""
+# Program to print square of all numbers in a list
+# print("Hello World")
+for i in range(0,99):
+	print("Helllo World")
